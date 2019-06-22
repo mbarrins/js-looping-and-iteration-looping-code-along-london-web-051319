d1d5801bafbcfa9ad0df89c4ef417c70cc570d88
mbarrins <maija.barrins@gmail.com>
2019-06-22 23:10:30 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(names, gift) {
+  const messages = [];
+  for (let i = 0; i < names.length; i++) {
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${gift} gift!`);
+  }
+  return messages;
+}
+
+function countdown( num ) {
+  while (num >= 0) {
+    console.log( num );
+    num -= 1
+  }
+
+}
